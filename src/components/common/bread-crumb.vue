c8ebcd49ef71f1ea5abb6dad27839697b240e328
Tungzhen <dongzz@aliyun.com>
2019-12-23 22:37:51 +0800

    黑马头条PC-面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 封装 el-breadcrumb 面包屑组件  -->
+  <!-- separator 分隔符 -->
+  <el-breadcrumb separator=">">
+  <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>
+      <!-- 定义一个具名插槽 -->
+      <slot name='title'></slot>
+  </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
