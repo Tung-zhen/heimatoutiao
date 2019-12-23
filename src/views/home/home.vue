7da0824f28a9f8adb9ab1a4d28ee7ce2e5dd29a3
Tungzhen <dongzz@aliyun.com>
2019-12-23 16:40:33 +0800

    黑马头条PC-主页模块-默认导航

---
+++ src/views/home/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-carousel height="800px" direction="vertical" :autoplay="false">
+    <el-carousel-item v-for="item in list" :key="item">
+      <img :src="item" alt="">
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      list: ['https://w.wallhaven.cc/full/4x/wallhaven-4x9jl3.jpg', 'https://w.wallhaven.cc/full/49/wallhaven-49opz8.png']
+    }
+  }
+}
+</script>
+
+<style>
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
