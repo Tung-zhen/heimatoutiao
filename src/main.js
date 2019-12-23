42be2734f55576e8dc3fb35eb3576c2683e6ab6d
Tungzhen <dongzz@aliyun.com>
2019-12-23 20:43:26 +0800

    黑马头条PC-接口访问-axios拦截器-统一处理请求token

--- src/main.js
+++ src/main.js
@@ -6,8 +6,7 @@ import ElementUI from 'element-ui' // 引入模块
 import Component from './components/index'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styles/index.less' // 引入初始化样式
-import axios from 'axios' // 引入axios
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+import axios from './utils/request'
 Vue.prototype.$axios = axios // 给axios赋值给全局属性
 Vue.use(ElementUI)
 Vue.use(Component)
