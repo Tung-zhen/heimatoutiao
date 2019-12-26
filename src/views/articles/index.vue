1ca9ea21fd895782c3e3622ef686c563baa1453f
Tungzhen <dongzz@aliyun.com>
2019-12-26 20:29:12 +0800

    黑马头条PC-内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card class="articles">
     <bread-crumb slot="header">
         <template slot="title">文章列表</template>
     </bread-crumb>
@@ -26,6 +26,25 @@
             <el-date-picker v-model="searchForm.dataRange" type="daterange" start-placeholder="开始日期" end-placeholder="结束日期"></el-date-picker>
         </el-form-item>
     </el-form>
+    <el-row class="totle" type="flex" align="middle">
+        <span>共找到1000条符合条件的内容</span>
+    </el-row>
+    <div class="article-item">
+        <!-- 左侧 -->
+        <div class="left">
+            <img src="../../assets/img/de.jpg" alt="">
+            <div class="info">
+                <span>哈哈哈哈哈哈哈哈哈哈哈</span>
+                <el-tag class="tag">标签一</el-tag>
+                <span class="date">2019</span>
+            </div>
+        </div>
+        <!-- 右侧 -->
+        <div class="right">
+            <span><i class="el-icon-edit"></i>修改</span>
+            <span><i class="el-icon-delete"></i>删除</span>
+        </div>
+    </div>
   </el-card>
 </template>
 
@@ -58,6 +77,47 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+    .articles {
+        .totle {
+            height: 60px;
+            border-bottom: 1px dashed #ccc;
+        }
+        .article-item {
+            display: flex;
+            justify-content: space-between;
+            padding: 20px 0;
+            border-bottom: 1px solid #f2f3f5;
+            .left {
+                display: flex;
+                img {
+                    width: 150px;
+                    height: 150px;
+                    border-radius: 4px;
+                }
+                .info {
+                height: 100px;
+                margin-left: 10px;
+                display: flex;
+                flex-direction: column; // 从上向下布局
+                justify-content: space-around;
+                .date {
+                    font-size: 12px;
+                    color: #999;
+                }
+                .tag {
+                    text-align: center;
+                    width: 60px;
+                    }
+                }
+            }
+            .right {
+                span {
+                    font-size: 14px;
+                    margin-right:8px;
+                    cursor: pointer; // 修改鼠标样式为小手
+                }
+            }
+        }
+    }
 </style>
